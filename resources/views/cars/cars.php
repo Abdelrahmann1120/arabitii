ea6c57058ad80e75161bd63c791c0468cc580559
unknown <abdelrahman.n91120@gmail.com>
2022-05-24 13:51:47 +0200

    first commit

---
+++ resources/views/cars/cars.php
@@ -0,0 +1,32 @@
+<style class="css"></style> 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Arabety</title>
+    <link rel="stylesheet" href="css/all.min.css">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+
+        <div class="">
+          <h1>car details</h1>
+
+          
+          <div class="row">
+            <div class="blog-post cccc">
+                <figure class="img">
+                    <img src="img/blog-item-01.png" alt="">
+                </figure>
+                <div class="blog-cont">
+                    <h3><a href="#">Vivamus ac vehicula dui</a></h3>
+                    <p class="text">Cras aliquet ligula dui, vitae fermentum velit tincidunt id. Praesent eu finibus nunc. Nulla in sagittis eros. Aliquam egestas augue.</p>
+                    <a href="car.html" class="main-btn">read more</a>
+                </div>
+            </div>
+            </div>
+        </div>
+</body>
+</html>
